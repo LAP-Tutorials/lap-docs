0d0fd8d9449b92a9e1d898a6f291b545bb9a660f
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-10-11 22:45:57 +0000

    post cards

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react'
-import {Layout} from '../components'
+import React, { useEffect, useState } from 'react'
+import { Layout } from '../components'
 
 import '@/styles/globals.css';
 
@@ -11,4 +11,4 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
     </Layout>
   )
-}
+} 
