538641cd2e7d4d95fd155fd40a980037a8f751b4
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-10-12 02:16:46 +0000

    complete post author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>Author</div>
+    <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20 border-2 border-[#2196f3]'>
+  <div className='absolute left-0 right-0 -top-14 mx-auto'>
+    <Image alt={author.name} unoptimized height="100" width="100" className='align-middle rounded-full border-2 border-[#2196f3]' src={author.photo.url} style={{ left: 0, right: 0, margin: 'auto' }} />
+  </div>
+  <h3 className='text-xl font-bold my-4'>{author.name}</h3>
+  <p className='text-lg'>{author.bio}</p>
+</div>
   )
 }
 
