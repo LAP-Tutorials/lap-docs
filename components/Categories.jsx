1c2b642d23e0271b4e5985d116cd37d15f8f091a
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-10-12 00:32:33 +0000

    article page added

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+
+import { getCategories } from '../services'
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories()
+      .then((newCategories) => setCategories(newCategories))
+  }, [])
   return (
-    <div>Categories</div>
+    <div className='bg-[#141413] shadow-lg shadow-[#2196f360] border-2 border-[#2196f3] rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b border-[#2196f3] pb-4'>
+        Categories
+      </h3>
+      {categories.map((category) => (
+        <Link href={`/category/${category.slug}`} key={category.slug}>
+          <span className="cursor-pointer block pb-3 mb-3 transition duration-700
+hover:text-[#2196f3]">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
   )
 }
 
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
-const PostWidget = () => {
+import { getRecentPosts, getSimilarPosts } from '../services'
+
+const PostWidget = ({ categories, slug}) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+if(slug) {
+  getSimilarPosts(categories, slug)
+    .then((result) => setRelatedPosts(result))
+} else {
+  getRecentPosts()
+  .then((result) => setRelatedPosts(result))
+}
+
+  }, [])
   return (
-    <div>PostWidget</div>
+    <div className='bg-[#141413] shadow-lg shadow-[#2196f360] border-2 border-[#2196f3] rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b border-[#2196f3] pb-4'>
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+    {relatedPosts.map((post) => (
+      <div key={post.title} className="flex items-center w-full mb-4">
+        <div className='w-16 flex-none'>
+          <img alt={post.title}
+          height="60px"
+          width="60px"
+          className='align-middle rounded'
+          src={post.featuredImage.url}
+          />
+        </div>
+        <div className="flex-grow ml-4">
+          <p className="text-[#d4d4d4] font-xs">
+            {moment(post.createdAt).format('MMM DD, YYYY')}
+          </p>
+          <Link href={`/post/${post.slug}`} key={post.title} className="text-md transition duration-700
+hover:text-[#2196f3]">
+            {post.title}
+          </Link>
+        </div>
+
+      </div>
+    ))}
+    </div>
   )
 }
 
