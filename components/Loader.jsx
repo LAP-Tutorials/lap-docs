729c3176c0802667dc90d5ee2a3761804344aa49
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-10-12 15:28:22 +0000

    fixed comment issue

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -4,7 +4,7 @@ const Loader = () => (
   <div className="text-center">
     <button
       type="button"
-      className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-rose-600 hover:bg-rose-500 focus:border-rose-700 active:bg-rose-700 transition ease-in-out duration-150 cursor-not-allowed"
+      className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-[#141413] hover:bg-[#141413] focus:border-[#2196f3] active:bg-[#2196f3] transition ease-in-out duration-150 cursor-not-allowed"
       disabled=""
     >
       <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -6,17 +6,20 @@ import { getRecentPosts, getSimilarPosts } from '../services'
 
 const PostWidget = ({ categories, slug}) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
+  const [fetched, setFetched] = useState(false);
 
   useEffect(() => {
-if(slug) {
-  getSimilarPosts(categories, slug)
-    .then((result) => setRelatedPosts(result))
-} else {
-  getRecentPosts()
-  .then((result) => setRelatedPosts(result))
-}
-
-  }, [])
+    if (!fetched) {
+    const fetchPosts = async () => {
+      const posts = slug
+        ? await getSimilarPosts(categories, slug)
+        : await getRecentPosts();
+      setRelatedPosts(posts);
+    };
+    fetchPosts();
+  }
+  }, [categories, slug]);
+  
   return (
     <div className='bg-[#141413] shadow-lg shadow-[#2196f360] border-2 border-[#2196f3] rounded-lg p-8 mb-8'>
       <h3 className='text-xl mb-8 font-semibold border-b border-[#2196f3] pb-4'>
@@ -37,7 +40,7 @@ if(slug) {
             {moment(post.createdAt).format('MMM DD, YYYY')}
           </p>
           <Link href={`/post/${post.slug}`} key={post.title} className="text-md transition duration-700
-hover:text-[#2196f3]">
+hover:text-[#2196f3]" >
             {post.title}
           </Link>
         </div>
