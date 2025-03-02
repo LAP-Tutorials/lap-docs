674e1ce1107f81b19183a0786b912ee4b06545d6
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2025-03-02 16:14:23 +0000

    update Netlify deployment configuration and add Next.js plugin

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",  // Ensures Next.js works on Netlify
-  experimental: {
-    appDir: true, // If using the App Router
-},
+
 };
 
 module.exports = nextConfig
