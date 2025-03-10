8d90d304d64a3ac6c12e4650ad26dc8b388ebc2b
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2025-03-10 17:48:13 +0000

    refactor: streamline markdown processing by removing hashtag handling in headings and enhancing sanitization logic

--- components/ArticleContent.tsx
+++ components/ArticleContent.tsx
@@ -12,37 +12,19 @@ const ArticleContent: React.FC<ArticleContentProps> = ({ content }) => {
   const [sanitizedContent, setSanitizedContent] = useState<string>("");
 
   useEffect(() => {
-    async function processContent() {
-      // Convert markdown to HTML with Marked and await the result
-      const rawHtml = await marked.parse(content);
+    async function sanitizeContent() {
+      // Convert markdown to HTML
+      const rawContent = await marked.parse(content);
 
-      // Remove #hashtags specifically from heading text using a regex on <hN> blocks
-      const htmlNoHashtagsInHeadings = rawHtml.replace(
-        /(<h[1-6][^>]*>)([^<]+)(<\/h[1-6]>)/g,
-        (match, openTag, headingText, closeTag) => {
-          // Remove all occurrences of #something
-          const textWithoutHashtags = headingText.replace(/#[^\s]+/g, "");
-          return `${openTag}${textWithoutHashtags}${closeTag}`;
-        }
-      );
-
-      // Sanitize final HTML
-      const cleanContent = DOMPurify.sanitize(htmlNoHashtagsInHeadings, {
+      // Configure DOMPurify to allow iframes
+      const cleanContent = DOMPurify.sanitize(rawContent, {
         ADD_TAGS: ["iframe"],
-        ADD_ATTR: [
-          "allow",
-          "allowfullscreen",
-          "frameborder",
-          "height",
-          "scrolling",
-          "src",
-          "width",
-        ],
+        ADD_ATTR: ["allow", "allowfullscreen", "frameborder", "height", "scrolling", "src", "width"],
       });
 
       setSanitizedContent(cleanContent);
     }
-    processContent();
+    sanitizeContent();
   }, [content]);
 
   return (
