bf4c8fc90d57639bc498e3bd8a4fd33dd8d15a67
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2025-12-27 10:46:39 +0000

    feat: add dynamic Open Graph image generation for main, posts, and team pages

--- app/team/opengraph-image.tsx
+++ app/team/opengraph-image.tsx
@@ -11,6 +11,15 @@ export const size = {
 export const contentType = "image/png";
 
 export default async function Image() {
+  const fontData = await fetch(
+    new URL("../../public/fonts/general-sans/GeneralSans-Semibold.woff2", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  // Fetch the PNG Logo
+  const logoData = await fetch(
+    new URL("../../public/logos/LAP-Logo-Transparent.png", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
   return new ImageResponse(
     (
       <div
@@ -22,45 +31,43 @@ export default async function Image() {
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          fontFamily: "sans-serif",
+          fontFamily: '"General Sans", sans-serif',
           position: "relative",
         }}
       >
-        <div style={{
-            width: "80px",
-            height: "80px",
-            background: "#9D4EDD",
-            marginBottom: "40px",
-            clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)",
-            display: "flex"
-        }} />
-
-        <div
-        style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            fontWeight: 900,
-        }}
-        >
-            <span style={{ color: "#fff", fontSize: 200, letterSpacing: "-0.05em", lineHeight: 0.8 }}>L.A.P</span>
-            <span style={{ color: "#9D4EDD", fontSize: 200, marginLeft: "40px", letterSpacing: "-0.05em", lineHeight: 0.8  }}>TEAM</span>
+        {/* PNG Logo - Scaled */}
+        <div style={{ marginBottom: "50px", display: "flex" }}>
+             <img
+                width="150"
+                height="150"
+                src={logoData as any}
+                alt="L.A.P Logo"
+            />
         </div>
-        
-        <div style={{ 
-            marginTop: "30px",
-            color: "#888", 
-            fontSize: 32, 
-            fontWeight: 500,
-            textAlign: "center",
-            letterSpacing: "0.05em"
-        }}>
-            Meet the Creators
+
+        {/* Real SVG Text (Authors.svg) */}
+        <div style={{ display: "flex" }}>
+          <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" width="800" height="121" viewBox="0 0 1520 231">
+            <path fill="#8a2be2" d="M114.1,223.8c-14.9,0-28.6-2.7-41.3-8-12.7-5.3-23.8-12.8-33.3-22.5-9.5-9.6-17-21-22.3-34-5.3-13.1-8-27.3-8-42.8s2.7-29.6,8-42.6c5.3-13,12.8-24.2,22.3-33.7s20.6-17,33.3-22.3c12.7-5.3,26.5-8,41.3-8s28.7,2.7,41.5,8c12.8,5.3,24,12.8,33.6,22.3,9.6,9.5,17.1,20.8,22.5,33.7,5.3,13,8,27.2,8,42.6s-2.7,29.7-8,42.8c-5.3,13.1-12.8,24.4-22.5,34-9.6,9.6-20.8,17.1-33.6,22.5-12.8,5.3-26.6,8-41.5,8ZM114.1,184.9c9.3,0,17.8-1.8,25.3-5.3,7.5-3.5,13.9-8.4,19.2-14.7,5.2-6.3,9.2-13.6,12-21.9,2.8-8.3,4.1-17.1,4.1-26.5s-1.4-18.1-4.1-26.3c-2.8-8.2-6.8-15.4-12-21.6-5.2-6.2-11.6-11.1-19.2-14.6-7.5-3.5-16-5.3-25.3-5.3s-17.7,1.8-25.2,5.3c-7.4,3.5-13.8,8.4-19,14.6-5.2,6.2-9.2,13.4-12,21.6-2.8,8.2-4.1,17-4.1,26.3s1.4,18.2,4.1,26.5c2.8,8.3,6.8,15.6,12,21.9,5.2,6.3,11.6,11.2,19,14.7,7.4,3.5,15.8,5.3,25.2,5.3Z"/>
+            <path fill="#8a2be2" d="M418.1,14.2v127.3c0,16.2-3.3,30.5-9.9,42.9-6.6,12.4-16.2,22.1-28.7,29-12.6,7-27.8,10.4-45.8,10.4s-33.5-3.5-46-10.4c-12.6-7-22.2-16.6-28.7-29-6.6-12.4-9.9-26.7-9.9-42.9V14.2h43.5v128.1c0,8.4,1.4,15.8,4.3,22.3,2.9,6.5,7.3,11.5,13.3,15,6,3.5,13.9,5.3,23.6,5.3s17.3-1.8,23.3-5.3c6-3.5,10.4-8.5,13.3-15,2.9-6.5,4.3-13.9,4.3-22.3V14.2h43.5Z"/>
+            <path fill="#8a2be2" d="M503.1,219.5h-43.5V14.2h80.4c25.9,0,46,5.8,60.3,17.3,14.3,11.5,21.5,28,21.5,49.3s-4.1,25.9-12.2,36.5c-8.1,10.6-19.5,18.3-34.2,23l52.9,79.2h-51.8l-46.3-72.6h-27.2v72.6ZM503.1,50.2v60.9h34.9c13.2,0,23.2-2.5,30-7.6,6.9-5,10.3-12.6,10.3-22.7s-3.4-18-10.3-23c-6.9-5-16.9-7.6-30-7.6h-34.9Z"/>
+            <path fill="#8a2be2" d="M807.1,219.5h-43.5V53.4h-64.6V14.2h173v39.2h-64.9v166.2Z"/>
+            <path fill="#8a2be2" d="M1036.2,181.8v37.8h-138.1V14.2h136.1v37.8h-92.7v43.2h84.1v37.5h-84.1v49.2h94.7Z"/>
+            <path fill="#8a2be2" d="M1207.2,219.5l-54.9-157.9-56.1,157.9h-43.8l77.2-205.3h47.5l76.6,205.3h-46.6ZM1106.8,140.9h90.9l11.4,34.6h-115l12.6-34.6Z"/>
+            <path fill="#8a2be2" d="M1371.9,219.5l-53.8-136.4v136.4h-39.5V14.2h52.1l59.5,152.2,59.8-152.2h52.1v205.3h-39.5V82l-54.3,137.6h-36.3Z"/>
+          </svg>
         </div>
       </div>
     ),
     {
       ...size,
+      fonts: [
+        {
+          name: "General Sans",
+          data: fontData,
+          style: "normal",
+        },
+      ],
     }
   );
 }
